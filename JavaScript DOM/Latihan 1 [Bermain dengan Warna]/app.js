85dee08fc9363801a764b56909932e3668f5e3e1
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-02-20 14:17:51 +0700

    mousemove Event Ubah Warna Background

--- JavaScript DOM/Latihan 1 [Bermain dengan Warna]/app.js
+++ JavaScript DOM/Latihan 1 [Bermain dengan Warna]/app.js
@@ -39,3 +39,9 @@ blueSlider.addEventListener("input", function () {
   const b = blueSlider.value;
   document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 });
+
+document.body.addEventListener("mousemove", function (event) {
+  const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+  const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+  document.body.style.backgroundColor = `rgb(${xPos},${yPos},100)`;
+});
