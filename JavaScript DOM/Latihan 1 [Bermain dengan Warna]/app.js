5ff85252f9ef8f2d68b88d9b311f4de1be67bcc3
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-02-20 11:45:13 +0700

    Tombol Acak Warna Background

--- JavaScript DOM/Latihan 1 [Bermain dengan Warna]/app.js
+++ JavaScript DOM/Latihan 1 [Bermain dengan Warna]/app.js
@@ -1,6 +1,19 @@
 const changeColor = document.getElementById("changeColor");
-change.onclick = function () {
+changeColor.onclick = function () {
   // document.body.style.backgroundColor = "lightblue";
   // document.body.setAttribute("class", "biru-muda");
   document.body.classList.toggle("biru-muda");
 };
+
+const randomColor = document.createElement("button");
+const textButton = document.createTextNode("Random Color");
+randomColor.appendChild(textButton);
+randomColor.setAttribute("type", "button");
+changeColor.after(randomColor);
+
+randomColor.addEventListener("click", function () {
+  const r = Math.round(Math.random() * 255 + 1);
+  const g = Math.round(Math.random() * 255 + 1);
+  const b = Math.round(Math.random() * 255 + 1);
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+});
