b52429bc1e87fb981573627ad968e5560712c8b7
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-02-21 14:14:25 +0700

    Pengujian Event Bubbling pada Tombol Close

--- JavaScript DOM/DOM Traversal/app.js
+++ JavaScript DOM/DOM Traversal/app.js
@@ -15,5 +15,13 @@ close.forEach(function (el) {
   el.addEventListener("click", function (e) {
     e.target.parentElement.style.display = "none";
     e.preventDefault();
+    e.stopPropagation();
+  });
+});
+
+const cards = document.querySelectorAll(".card");
+cards.forEach(function (card) {
+  card.addEventListener("click", function (e) {
+    alert("ok");
   });
 });
