048f9a1fa84234e51ee63c84660bc641d9a38b85
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-03-20 15:12:00 +0700

    Add Modal to See Movie Details

--- REST API/Project 2 [Movie Search App]/js/script.js
+++ REST API/Project 2 [Movie Search App]/js/script.js
@@ -23,7 +23,9 @@ function searchMovie() {
                   <h6 class="card-subtitle mb-2 text-muted">` +
               data.Year +
               `</h6>
-              <a href="#" class="card-link">See Details</a>
+              <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id=` +
+              data.imdbID +
+              `>See Details</a>
                 </div>
               </div>
             </div>`
@@ -52,4 +54,51 @@ $("#search-input").on("keyup", function (e) {
   if (e.which === 13) {
     searchMovie();
   }
+});
+
+$("#movie-list").on("click", ".see-detail", function () {
+  $.ajax({
+    url: "http://omdbapi.com",
+    type: "get",
+    dataType: "json",
+    data: { apikey: "b8ccd72d", i: $(this).data("id") },
+    success: function (movie) {
+      if (movie.Response === "True") {
+        $(".modal-body").html(
+          `
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="` +
+            movie.Poster +
+            `" class="img-fluid">
+              </div>
+              <div class="col-md-8">
+                <ul class="list-group">
+                  <li class="list-group-item"><h3>` +
+            movie.Title +
+            `</h3></li>
+                  <li class="list-group-item">Released: ` +
+            movie.Released +
+            `</li>
+                  <li class="list-group-item">Genre: ` +
+            movie.Genre +
+            `</li>
+                  <li class="list-group-item">Director: ` +
+            movie.Director +
+            `</li>
+                  <li class="list-group-item">Actor: ` +
+            movie.Actors +
+            `</li>
+                  <li class="list-group-item">Rating: ` +
+            movie.imdbRating +
+            `</li>
+                </ul>
+              </div>
+            </div>
+          </div>`
+        );
+      }
+    },
+  });
 });
