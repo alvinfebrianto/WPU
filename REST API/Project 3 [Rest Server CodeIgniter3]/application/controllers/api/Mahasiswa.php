24a1f49788de33e9f4f61e92b60eb7c141b1d438
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-03-22 09:31:26 +0700

    Rest Server CodeIgniter3

---
+++ REST API/Project 3 [Rest Server CodeIgniter3]/application/controllers/api/Mahasiswa.php
@@ -0,0 +1,79 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Mahasiswa extends REST_Controller {
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('Mahasiswa_model', 'mahasiswa');
+  }
+
+  public function index_get() {
+    $id = $this->get('id');
+    if($id === null) {
+      $mahasiswa = $this->mahasiswa->getMahasiswa();
+    } else {
+      $mahasiswa = $this->mahasiswa->getMahasiswa($id);
+    }
+    
+    if($mahasiswa) {
+      $this->response([
+        'status' => TRUE,
+        'data' => $mahasiswa
+      ], REST_Controller::HTTP_OK);
+    } else {
+      $this->response([
+        'status' => FALSE,
+        'message' => 'id not found'
+      ], REST_Controller::HTTP_NOT_FOUND);
+    }
+  }
+
+  public function index_delete() {
+    $id = $this->delete('id');
+    if($id === null) {
+      $this->response([
+        'status' => FALSE,
+        'message' => 'provide an id!'
+      ], REST_Controller::HTTP_BAD_REQUEST);
+    } else {
+      if($this->mahasiswa->deleteMahasiswa($id) > 0) {
+        $this->response([
+          'status' => TRUE,
+          'id' => $id,
+          'message' => 'successfully deleted.'
+        ], REST_Controller::HTTP_NO_CONTENT);
+      } else {
+        $this->response([
+          'status' => FALSE,
+          'message' => 'id not found!'
+        ], REST_Controller::HTTP_BAD_REQUEST);
+      }
+    }
+  }
+
+  public function index_post() {
+    $data = [
+      'nrp' => $this->post('nrp'),
+      'nama' => $this->post('nama'),
+      'email' => $this->post('email'),
+      'jurusan' => $this->post('jurusan')
+    ];
+
+    if($this->mahasiswa->createMahasiswa($data) > 0) {
+      $this->response([
+        'status' => TRUE,
+        'message' => 'new mahasiswa has been created.'
+      ], REST_Controller::HTTP_CREATED);
+    } else {
+      $this->response([
+        'status' => FALSE,
+        'message' => 'failed to create new data!'
+      ], REST_Controller::HTTP_BAD_REQUEST);
+    }
+  }
+}
