46956706e3e8b793c807373d0e7d4e54853255cf
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-04-23 16:11:04 +0700

    Functions & Directives - Core Concepts | Tailwind CSS

---
+++ Tailwind CSS/3-Functions-and-Directives/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing: { 13: "3.25rem" },
+      fontFamily: { inter: ["Inter"] },
+      colors: { kopi: "#bada55" },
+      animation: {
+        "spin-slow": "spin 2s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
