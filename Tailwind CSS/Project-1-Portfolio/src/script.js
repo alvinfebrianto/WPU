cfd1f5e959a5f0e5d270eb2bb43bc5e2e6c8e13b
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-04-29 10:31:48 +0700

    Dark:Navbar - Project 1 Portfolio Tailwind CSS

--- Tailwind CSS/Project-1-Portfolio/src/script.js
+++ Tailwind CSS/Project-1-Portfolio/src/script.js
@@ -1,16 +1,44 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  // const toTop = document.querySelector("#to-top");
+
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    // toTop.classList.remove("hidden");
+    // toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    // toTop.classList.remove("flex");
+    // toTop.classList.add("hidden");
   }
 };
 
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
-hamburger.addEventListener("click", function () {
+
+// Event listener untuk mengaktifkan atau menonaktifkan hamburger
+hamburger.addEventListener("click", function (e) {
+  e.stopPropagation();
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Event listener untuk menutup hamburger saat mengklik di luar hamburger atau menu navigasi
+window.addEventListener("click", function (e) {
+  if (e.target !== hamburger && e.target !== navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
 });
