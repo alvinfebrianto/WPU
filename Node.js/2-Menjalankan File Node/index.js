39c9cb1c1ae3e9d2b526b39391893d73c7e681ca
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-11-02 15:00:06 +0700

    2 Menjalankan File Node [require & exports] | Node.js

---
+++ Node.js/2-Menjalankan File Node/index.js
@@ -0,0 +1,9 @@
+// const nama = "Alvin Febrianto";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+// require("./script");
+// console.log("Hello Alvin!");
+
+const cetakNama = require("./script");
+console.log(cetakNama("Alvin"));
---
+++ Node.js/2-Menjalankan File Node/script.js
@@ -0,0 +1,6 @@
+// console.log("Hello World!");
+
+module.exports = cetakNama;
+function cetakNama(nama) {
+  return `Halo, nama saya ${nama}`;
+}
