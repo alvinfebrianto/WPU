a171b5a1c717873e561e26cd9aacc7a3c3051628
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-11-15 07:40:11 +0700

    4 Node.js Core Module [Readline input and save answer to JSON file using readFileSync & writeFileSync] | Node.js

--- Node.js/4-Node.js Core Module/app.js
+++ Node.js/4-Node.js Core Module/app.js
@@ -33,7 +33,12 @@ const rl = readline.createInterface({
 });
 rl.question("Masukkan nama Anda: ", (nama) => {
   rl.question("Masukkan no HP Anda: ", (noHP) => {
-    console.log(`Terima kasih ${nama}, sudah menginputkan ${noHP}`);
+    const contact = { nama, noHP };
+    const file = fs.readFileSync("data/contacts.json", "utf-8");
+    const contacts = JSON.parse(file);
+    contacts.push(contact);
+    fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+    console.log("Terima kasih sudah memasukkan data");
     rl.close();
   });
 });
