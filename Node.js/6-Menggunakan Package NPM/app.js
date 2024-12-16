ba5d30f7e2c78e4bc0ee1c167557dd95b167cab1
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-12-16 19:49:50 +0700

    6 Menggunakan Package NPM [nodemon] | Node.js

--- Node.js/6-Menggunakan Package NPM/app.js
+++ Node.js/6-Menggunakan Package NPM/app.js
@@ -2,6 +2,6 @@ const chalk = require("chalk");
 
 console.log(chalk.blue("Hello World!"));
 console.log(chalk.italic("Hello World!"));
-console.log(chalk.black.bgBlue("Hello World!"));
+console.log(chalk.black.bgGreen("Hello World!"));
 const pesan = chalk`{bgRed Alvin} Febrianto`;
 console.log(pesan);
