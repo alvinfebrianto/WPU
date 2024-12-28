1274521cc5e0009353d00a598b2cd05d9d445a6e
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-12-28 23:22:42 +0700

    7 Contact App [yargs] | Node.js

--- Node.js/7-Contact App/app.js
+++ Node.js/7-Contact App/app.js
@@ -1,11 +1,13 @@
-const { tulisPertanyaan, simpanContact } = require("./contacts");
+const yargs = require("yargs");
 
-const main = async () => {
-  const nama = await tulisPertanyaan("Masukkan nama Anda: ");
-  const email = await tulisPertanyaan("Masukkan email Anda: ");
-  const noHP = await tulisPertanyaan("Masukkan nomor HP Anda: ");
+// const { tulisPertanyaan, simpanContact } = require("./contacts");
 
-  simpanContact(nama, email, noHP);
-}
+// const main = async () => {
+//   const nama = await tulisPertanyaan("Masukkan nama Anda: ");
+//   const email = await tulisPertanyaan("Masukkan email Anda: ");
+//   const noHP = await tulisPertanyaan("Masukkan nomor HP Anda: ");
 
-main();
+//   simpanContact(nama, email, noHP);
+// }
+
+// main();
