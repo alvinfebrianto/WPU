7ea3a8961c147edb6179adde2e371cebf1c56e19
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2024-12-31 17:13:27 +0700

    7 Contact App [implement yargs command builder for add contact] | Node.js

--- Node.js/7-Contact App/app.js
+++ Node.js/7-Contact App/app.js
@@ -1,13 +1,40 @@
 const yargs = require("yargs");
 
-// const { tulisPertanyaan, simpanContact } = require("./contacts");
+// yargs.command(
+//   'add',
+//   'Menambahkan contact baru',
+//   () => {},
+//   (argv) => {console.log(argv.nama);}
+// );
 
-// const main = async () => {
-//   const nama = await tulisPertanyaan("Masukkan nama Anda: ");
-//   const email = await tulisPertanyaan("Masukkan email Anda: ");
-//   const noHP = await tulisPertanyaan("Masukkan nomor HP Anda: ");
+yargs.command({
+  command: 'add',
+  describe: 'Menambahkan cntact baru',
+  builder: {
+    nama: {
+      describe: 'Nama Lengkap',
+      demandOption: true,
+      type: 'string',
+    },
+    email: {
+      describe: 'Email',
+      demandOption: false,
+      type: 'string',
+    },
+    noHP: {
+      describe: 'Nomor Handphone',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    const contact = {
+      nama: argv.nama,
+      email: argv.email,
+      noHP: argv.nohp,
+    };
+    console.log(contact)
+  },
+});
 
-//   simpanContact(nama, email, noHP);
-// }
-
-// main();
+yargs.parse();
